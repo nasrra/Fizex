5200f42d44ff963fa87c4fca0208fee50559e1cd
nasrra <naserra.work@gmail.com>
2026-08-11 13:22:31 +1000

    win32 build now possible with .bat file.

---
+++ build_debug_win32.bat
@@ -0,0 +1,11 @@
+@REM
+@REM     For win32 platforrm layer.
+@REM     - user32.lib
+@REM     - Gdi32.lib
+@REM     For wgpu_native.lib as it is rust-lang bindings, and rust contains those libs in its standard lib.
+@REM     - ws2_32.lib 
+@REM     - ntdll.lib 
+@REM     - userenv.lib
+@REM
+
+mkdir build & cd src & cl /Zi /W3 /WX /Fo:..\\build/ /Fd:..\\build/ /Fe:..\\build/app.exe app_win32.c app.c base_layer\\base.c user32.lib Gdi32.lib ws2_32.lib ntdll.lib userenv.lib vendors\\webgpu\\wgpu_native.lib
